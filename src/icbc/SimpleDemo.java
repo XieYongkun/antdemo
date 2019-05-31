4fb6a224a8764d932a6287915e91f7165fde92d1
admin <332625228@qq.com>
2019-05-31 17:12:26 +0800

    antDemo

---
+++ src/icbc/SimpleDemo.java
@@ -0,0 +1,10 @@
+package icbc;
+
+public class SimpleDemo {
+	 public int Add(int a,int b){        
+	    return (a+b);        
+	}
+	 public int Method(int a){        
+		    return (a);        
+		}
+}
